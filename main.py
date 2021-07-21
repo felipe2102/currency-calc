5d8670045b65516481b3390681ed0125ea1def17
Felipe Ribeiro <felipinhooliveira06@gmail.com>
2021-07-21 20:55:05 -0300

    Refatoração
    Simplifiquei o código porque tava muito feio

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+# Bibliotecas usadas
 import json
 from PyQt5 import uic, QtWidgets
 from PyQt5.QtWidgets import *
 import requests
 
 def calcular():
+	# Verifica se a quantidade é diferente de 0 ou ''
 	moeda_selec = home.comboBox.currentText()
 	quantidade = home.lineEdit.text()
 	if quantidade != '':
@@ -13,87 +15,64 @@ def calcular():
 			quantidade = float(quantidade)
 	else:
 		quantidade = 1
+
+	# Verifica qual a moeda e chama uma função
 	if moeda_selec == "Dólar":
-		Dolar = dolar()
-		result = float(Dolar) * float(quantidade)
+		Moeda = moeda("USD")
+		result = float(Moeda) * float(quantidade)
 		result = str(result)
 		home.label_resultado.setText(result)
 	elif moeda_selec == "Euro":
-		Euro = euro()
-		result = float(Euro) * float(quantidade)
+		Moeda = moeda("EUR")
+		result = float(Moeda) * float(quantidade)
 		result = str(result)
 		home.label_resultado.setText(result)
 	elif moeda_selec == "Libra":
-		Libra = libra()
-		result = float(Libra) * float(quantidade)
+		Moeda = moeda("GBP")
+		result = float(Moeda) * float(quantidade)
 		result = str(result)
 		home.label_resultado.setText(result)
 	elif moeda_selec == "Bitcoin":
-		BTC = btc()
-		result = float(BTC) * float(quantidade)
+		Moeda = moeda("BTC")
+		result = float(Moeda) * float(quantidade)
 		result = str(result)
 		home.label_resultado.setText(result)
 	elif moeda_selec == "Iene":
-		Iene = iene()
-		result = float(Iene) * float(quantidade)
+		Moeda = moeda("JPY")
+		result = float(Moeda) * float(quantidade)
 		result = str(result)
 		home.label_resultado.setText(result)
 	elif moeda_selec == "Dólar Canadense":
-		Dolca = dolca()
-		result = float(Dolca) * float(quantidade)
+		Moeda = moeda("CAD")
+		result = float(Moeda) * float(quantidade)
 		result = str(result)
 		home.label_resultado.setText(result)
 	elif moeda_selec == "Peso Argentino":
-		Pesoarg = pesoarg()
-		result = float(Pesoarg) * float(quantidade)
+		Moeda = moeda("ARS")
+		result = float(Moeda) * float(quantidade)
 		result = str(result)
 		home.label_resultado.setText(result)
 	else:
 		print("Não foi possivel pegar a moeda '-' ")
 
-def dolar():
-	request = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL")
+# Pega os dados da API e retorna para o programa
+def moeda(currency):
+	request = requests.get("https://economia.awesomeapi.com.br/last/"+currency+"-BRL")
 	moeda = json.loads(request.content)
-	moeda = moeda["USDBRL"]["high"]
+	moeda = moeda[currency + "BRL"]["high"]
 	return moeda
 
-def euro():
-	request = requests.get("https://economia.awesomeapi.com.br/last/EUR-BRL")
-	moeda = json.loads(request.content)
-	return moeda["EURBRL"]["high"]
-
-def btc():
-	request = requests.get("https://economia.awesomeapi.com.br/last/BTC-BRL")
-	moeda = json.loads(request.content)
-	return moeda["BTCBRL"]["high"]
-
-def libra():
-	request = requests.get("https://economia.awesomeapi.com.br/last/GBP-BRL")
-	moeda = json.loads(request.content)
-	return moeda["GBPBRL"]["high"]
-
-def iene():
-	request = requests.get("https://economia.awesomeapi.com.br/last/JPY-BRL")
-	moeda = json.loads(request.content)
-	return moeda["JPYBRL"]["high"]
-
-def dolca():
-	request = requests.get("https://economia.awesomeapi.com.br/last/CAD-BRL")
-	moeda = json.loads(request.content)
-	return moeda["CADBRL"]["high"]
-
-def pesoarg():
-	request = requests.get("https://economia.awesomeapi.com.br/last/ARS-BRL")
-	moeda = json.loads(request.content)
-	return moeda["ARSBRL"]["high"]
-
+# Cria o app Qt
 app = QtWidgets.QApplication([])
 
+# Carrega a interface
 home = uic.loadUi("home.ui")
 
+# Define o uso dos botões
 home.Button_calcular.clicked.connect(calcular)
 home.Button_sair.clicked.connect(home.close)
 
+# Mostra a tela e executa o aplicativo
 home.show()
 
 app.exec()
